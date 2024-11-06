a17031ac438d3666f6ff1e830dabf20a86b0775f
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-11-06 19:57:51 +0530

    samesite add

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -77,7 +77,8 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
 
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none",
     }
 
     return res.status(200)
